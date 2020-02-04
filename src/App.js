2ba24a68f9d0c4b2c120197360455a452d2a3d8c
babuy45 <ashabibi49@gmail.com>
2020-02-04 19:12:31 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Registration from './Pages/Registration';
+import Login from './Pages/Login';
+import ForgetPass from './Pages/ForgetPass';
+import NotFound404 from './Pages/NotFound404'; 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route path='/registration' component={Registration} />
+      <Route path='/login' component={Login} />
+      <Route path='/forget-pass' component={ForgetPass} />
+      <Route  component={NotFound404} />
+
+    </Router>
+  
   );
 }
 
